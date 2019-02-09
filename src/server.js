42dad86dd61fa0e3b2bb8c8883dd8c1b237f8c12
teslatickles <hunterhartline@gmail.com>
2019-02-09 16:25:01 -0600

    fixing heroku deploy

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
-'use strict'
-
 const app = require("./app");
 const http = require("http");
 
-const port = normalizePort(process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 const server = http.createServer(app);
