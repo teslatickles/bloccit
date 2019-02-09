d68cc740b78357544f17c6a50426cf33f3c25db2
teslatickles <hunterhartline@gmail.com>
2019-02-09 15:15:05 -0600

    add a trivial route with testing

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -11,4 +11,12 @@ describe("routes : static", () => {
             });
         });
     });
+    describe("GET /marco", () => {
+        it("should return status code 200", (done) => {
+            request.get(base + "marco", (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
 });
