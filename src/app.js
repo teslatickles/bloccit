fd387b75882c798b571dfa11f3172b835be88d4c
teslatickles <hunterhartline@gmail.com>
2019-02-09 15:52:27 -0600

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@
 
 const express = require("express");
 const app = express();
+const appConfig = require("./config/main-config");
 const routeConfig = require("./config/route-config");
 
+appConfig.init();
 routeConfig.init(app);
 
 module.exports = app;
