ce5352cbc741c39a766016670601188e538264f0
teslatickles <hunterhartline@gmail.com>
2019-02-11 20:47:14 -0600

    add env.js and db-config

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,8 @@
 const env = {
-    database: 'd19it75c9j04j2',
-    username: 'lxknzbbcpmmdyl',
-    password: '3f1a281360fc9c4a5c42f35efeb94643064caa4293c74b136e885cc7c3d57beb',
-    host: 'ec2-54-163-245-64.compute-1.amazonaws.com',
+    database: 'd5sel9598j3nbk',
+    username: 'vlpjwsnjdondlb',
+    password: '7d8cdd09ff1f5f847bc00af26ed0a2098fe2c737fc93dbc767952281a77e953f',
+    host: 'ec2-54-243-128-95.compute-1.amazonaws.com',
     port: 5432,
     dialect: 'postgres',
     pool: {
