772735c2b55f45f023ee2a48d0baa4c6c9570513
teslatickles <hunterhartline@gmail.com>
2019-02-12 00:22:58 -0600

    add back process.env[config...] in index.js

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -9,7 +9,7 @@ var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
 
 if (config.use_env_variable) {
-    var sequelize = new Sequelize(config.use_env_variable, config);
+    var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
     // eslint-disable-next-line no-redeclare
     var sequelize = new Sequelize(config.database, config.username, config.password, config);
