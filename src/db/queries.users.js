36d75a837ae99acea9e79cca28117655e7373433
teslatickles <hunterhartline@gmail.com>
2019-02-18 17:47:14 -0600

    add signin signup sign off views and logic

---
+++ src/db/queries.users.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-sync */
+const User = require("./models").User;
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+    createUser(newUser, callback) {
+        const salt = bcrypt.genSaltSync();
+        const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+
+        return User.create({
+            email: newUser.email,
+            password: hashedPassword
+        })
+            .then((user) => {
+                callback(null, user);
+            })
+            .catch((err) => {
+                callback(err);
+            })
+    }
+}
