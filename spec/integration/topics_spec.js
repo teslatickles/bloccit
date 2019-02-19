cd5f03d035d981fe41408d440011695feec8a2a5
teslatickles <hunterhartline@gmail.com>
2019-02-19 17:29:23 -0600

    add authorization logic

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -24,44 +24,86 @@ describe("routes: topics", () => {
                 })
         });
     });
-    describe("GET /topics", () => {
-
-        it("should return a status code 200 and all topics", (done) => {
-            request.get(base, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-                expect(err).toBeNull();
-                expect(body).toContain("Topics");
-                expect(body).toContain("JS Frameworks");
+    // admin user context
+    describe("admin user performing CRUD actions for Topic", () => {
+        beforeEach((done) => {
+            // User.create({
+            //     email: "admin@example.com",
+            //     password: "123456",
+            //     role: "admin"
+            // })
+            //     .then((user) => {
+            request.get({         // mock authentication
+                url: "http://localhost:3000/auth/fake",
+                form: {
+                    role: "admin",     // mock authenticate as admin user
+                    // userId: user.id,
+                    // email: user.email
+                }
+            }),
+                // (err, res, body) => {
                 done();
+            //     }
+            // );
+            // });
+        });
+        describe("GET /topics", () => {
+            it("should return a status code 200 and all topics", (done) => {
+                request.get(base, (err, res, body) => {
+                    expect(res.statusCode).toBe(200);
+                    expect(err).toBeNull();
+                    expect(body).toContain("Topics");
+                    expect(body).toContain("JS Frameworks");
+                    done();
+                });
             });
         });
-    });
-    describe("GET /topics/new", () => {
-        it("should render a new topic form", (done) => {
-            request.get(`${base}new`, (err, res, body) => {
-                expect(err).toBeNull();
-                expect(body).toContain("New Topic");
-                done();
+        describe("GET /topics/new", () => {
+            it("should render a new topic form", (done) => {
+                request.get(`${base}new`, (err, res, body) => {
+                    expect(err).toBeNull();
+                    expect(body).toContain("New Topic");
+                    done();
+                });
             });
         });
-    });
-    describe("GET /topics/create", () => {
-        const options = {
-            url: `${base}create`,
-            form: {
-                title: "Sonic Youth songs",
-                description: "What's your favorite Sonic Youth song?"
-            }
-        };
+        describe("GET /topics/create", () => {
+            const options = {
+                url: `${base}create`,
+                form: {
+                    title: "Sonic Youth songs",
+                    description: "What's your favorite Sonic Youth song?"
+                }
+            };
 
-        it("should create a new topic and redirect", (done) => {
-            request.post(options,
-                (err, res, body) => {
-                    Topic.findOne({ where: { title: "Sonic Youth songs" } })
+            it("should create a new topic and redirect", (done) => {
+                request.post(options,
+                    (err, res, body) => {
+                        Topic.findOne({ where: { title: "Sonic Youth songs" } })
+                            .then((topic) => {
+                                expect(res.statusCode).toBe(303);
+                                expect(topic.title).toBe("Sonic Youth songs");
+                                expect(topic.description).toBe("What's your favorite Sonic Youth song?");
+                                done();
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                                done();
+                            });
+                    });
+            });
+            it("should not create a new post that fails validations", (done) => {
+                const options = {
+                    url: `${base}create`,
+                    form: {
+                        title: "a",
+                        description: "b"
+                    }
+                };
+                request.post(options, (err, res, body) => {
+                    Topic.findOne({ where: { title: "a" } })
                         .then((topic) => {
-                            expect(res.statusCode).toBe(303);
-                            expect(topic.title).toBe("Sonic Youth songs");
-                            expect(topic.description).toBe("What's your favorite Sonic Youth song?");
+                            expect(topic).toBeNull();
                             done();
                         })
                         .catch((err) => {
@@ -69,87 +111,211 @@ describe("routes: topics", () => {
                             done();
                         });
                 });
+            });
         });
-        it("should not create a new post that fails validations", (done) => {
+        describe("GET /topics/:id", () => {
+
+            it("should render a view with the selected topic", (done) => {
+                request.get(`${base}${this.topic.id}`, (err, res, body) => {
+                    expect(err).toBeNull();
+                    expect(body).toContain("JS Frameworks");
+                    done();
+                });
+            });
+        });
+        describe("POST /topics/:id/destroy", () => {
+            it("should delete the topic with the associated ID", (done) => {
+                Topic.all()
+                    .then((topics) => {
+                        const topicCountBeforeDelete = topics.length;
+                        expect(topicCountBeforeDelete).toBe(1);
+
+                        request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
+                            Topic.all()
+                                .then((topics) => {
+                                    expect(err).toBeNull();
+                                    expect(topics.length).toBe(topicCountBeforeDelete - 1);
+                                    done();
+                                })
+                        });
+                    });
+            });
+        });
+        describe("GET /topics/:id/edit", () => {
+            it("should render a view with an edit topic form", (done) => {
+                request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
+                    expect(err).toBeNull();
+                    expect(body).toContain("Edit Topic");
+                    expect(body).toContain("JS Frameworks");
+                    done();
+                });
+            });
+        });
+        describe("POST /topics/:id/update", () => {
+            it("should update the topic with the given values", (done) => {
+                const options = {
+                    url: `${base}${this.topic.id}/update`,
+                    form: {
+                        title: "JavaScript Frameworks",
+                        description: "There are a lot of them"
+                    }
+                };
+                request.post(options,
+                    (err, res, body) => {
+                        expect(err).toBeNull;
+
+                        Topic.findOne({
+                            where: { id: this.topic.id }
+                        })
+                            .then((topic) => {
+                                expect(topic.title).toBe("JavaScript Frameworks");
+                                done();
+                            });
+                    });
+            });
+        });
+    });
+    // member user context
+    describe("member user performing CRUD actions for Topic", () => {
+        beforeEach((done) => {
+            request.get({
+                url: "http://localhost:3000/auth/fake",
+                form: {
+                    role: "member"
+                }
+            });
+            // (err, res, body) => {
+            done();
+            //     }
+            // );
+        });
+        describe("GET /topics", () => {
+
+            it("should return a status code 200 and all topics", (done) => {
+                request.get(base, (err, res, body) => {
+                    expect(res.statusCode).toBe(200);
+                    expect(err).toBeNull();
+                    expect(body).toContain("Topics");
+                    expect(body).toContain("JS Frameworks");
+                    done();
+                });
+            });
+        });
+        describe("GET /topics/new", () => {
+            it("should render a new topic form", (done) => {
+                request.get(`${base}new`, (err, res, body) => {
+                    expect(err).toBeNull();
+                    expect(body).toContain("New Topic");
+                    done();
+                });
+            });
+        });
+        describe("GET /topics/create", () => {
             const options = {
                 url: `${base}create`,
                 form: {
-                    title: "a",
-                    description: "b"
+                    title: "Sonic Youth songs",
+                    description: "What's your favorite Sonic Youth song?"
                 }
             };
-            request.post(options, (err, res, body) => {
-                Topic.findOne({ where: { title: "a" } })
-                    .then((topic) => {
-                        expect(topic).toBeNull();
-                        done();
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                        done();
-                    });
-            });
-        });
-    });
-    describe("GET /topics/:id", () => {
 
-        it("should render a view with the selected topic", (done) => {
-            request.get(`${base}${this.topic.id}`, (err, res, body) => {
-                expect(err).toBeNull();
-                expect(body).toContain("JS Frameworks");
-                done();
-            });
-        });
-    });
-    describe("POST /topics/:id/destroy", () => {
-        it("should delete the topic with the associated ID", (done) => {
-            Topic.all()
-                .then((topics) => {
-                    const topicCountBeforeDelete = topics.length;
-                    expect(topicCountBeforeDelete).toBe(1);
-
-                    request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
-                        Topic.all()
-                            .then((topics) => {
-                                expect(err).toBeNull();
-                                expect(topics.length).toBe(topicCountBeforeDelete - 1);
+            it("should create a new topic and redirect", (done) => {
+                request.post(options,
+                    (err, res, body) => {
+                        Topic.findOne({ where: { title: "Sonic Youth songs" } })
+                            .then((topic) => {
+                                expect(res.statusCode).toBe(303);
+                                expect(topic.title).toBe("Sonic Youth songs");
+                                expect(topic.description).toBe("What's your favorite Sonic Youth song?");
                                 done();
                             })
+                            .catch((err) => {
+                                console.log(err);
+                                done();
+                            });
                     });
-                });
-        });
-    });
-    describe("GET /topics/:id/edit", () => {
-        it("should render a view with an edit topic form", (done) => {
-            request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
-                expect(err).toBeNull();
-                expect(body).toContain("Edit Topic");
-                expect(body).toContain("JS Frameworks");
-                done();
             });
-        });
-    });
-    describe("POST /topics/:id/update", () => {
-        it("should update the topic with the given values", (done) => {
-            const options = {
-                url: `${base}${this.topic.id}/update`,
-                form: {
-                    title: "JavaScript Frameworks",
-                    description: "There are a lot of them"
-                }
-            };
-            request.post(options,
-                (err, res, body) => {
-                    expect(err).toBeNull;
-
-                    Topic.findOne({
-                        where: { id: this.topic.id }
-                    })
+            it("should not create a new post that fails validations", (done) => {
+                const options = {
+                    url: `${base}create`,
+                    form: {
+                        title: "a",
+                        description: "b"
+                    }
+                };
+                request.post(options, (err, res, body) => {
+                    Topic.findOne({ where: { title: "a" } })
                         .then((topic) => {
-                            expect(topic.title).toBe("JavaScript Frameworks");
+                            expect(topic).toBeNull();
+                            done();
+                        })
+                        .catch((err) => {
+                            console.log(err);
                             done();
                         });
                 });
+            });
+        });
+        describe("GET /topics/:id", () => {
+
+            it("should render a view with the selected topic", (done) => {
+                request.get(`${base}${this.topic.id}`, (err, res, body) => {
+                    expect(err).toBeNull();
+                    expect(body).toContain("JS Frameworks");
+                    done();
+                });
+            });
+        });
+        describe("POST /topics/:id/destroy", () => {
+            it("should delete the topic with the associated ID", (done) => {
+                Topic.all()
+                    .then((topics) => {
+                        const topicCountBeforeDelete = topics.length;
+                        expect(topicCountBeforeDelete).toBe(1);
+
+                        request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
+                            Topic.all()
+                                .then((topics) => {
+                                    expect(err).toBeNull();
+                                    expect(topics.length).toBe(topicCountBeforeDelete - 1);
+                                    done();
+                                })
+                        });
+                    });
+            });
+        });
+        describe("GET /topics/:id/edit", () => {
+            it("should render a view with an edit topic form", (done) => {
+                request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
+                    expect(err).toBeNull();
+                    expect(body).toContain("Edit Topic");
+                    expect(body).toContain("JS Frameworks");
+                    done();
+                });
+            });
+        });
+        describe("POST /topics/:id/update", () => {
+            it("should update the topic with the given values", (done) => {
+                const options = {
+                    url: `${base}${this.topic.id}/update`,
+                    form: {
+                        title: "JavaScript Frameworks",
+                        description: "There are a lot of them"
+                    }
+                };
+                request.post(options,
+                    (err, res, body) => {
+                        expect(err).toBeNull;
+
+                        Topic.findOne({
+                            where: { id: this.topic.id }
+                        })
+                            .then((topic) => {
+                                expect(topic.title).toBe("JavaScript Frameworks");
+                                done();
+                            });
+                    });
+            });
         });
     });
 });
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,6 +1,7 @@
 const Topic = require("./models").Topic;
 const Post = require("./models").Post;
 const Flair = require("./models").Flair;
+const Authorizer = require("../policies/topic");
 
 module.exports = {
     getAllTopics(callback) {
@@ -45,32 +46,57 @@ module.exports = {
                 callback(err);
             })
     },
-    deleteTopic(id, callback) {
-        return Topic.destroy({
-            where: { id }
-        })
+    deleteTopic(req, callback) {
+
+        // #1
+        return Topic.findById(req.params.id)
             .then((topic) => {
-                callback(null, topic);
+                // #2
+                const authorized = new Authorizer(req.user, topic).destroy();
+                if (authorized) {
+                    // #3
+                    topic.destroy()
+                        .then((res) => {
+                            callback(null, topic);
+                        });
+                } else {
+                    // #4
+                    req.flash("notice", "You are not authorized to do that.")
+                    callback(401);
+                }
             })
             .catch((err) => {
                 callback(err);
-            })
+            });
     },
-    updateTopic(id, updatedTopic, callback) {
-        return Topic.findById(id)
+    updateTopic(req, updatedTopic, callback) {
+
+        // #1
+        return Topic.findById(req.params.id)
             .then((topic) => {
+                // #2
                 if (!topic) {
                     return callback("Topic not found");
                 }
-                topic.update(updatedTopic, {
-                    fields: Object.keys(updatedTopic)
-                })
-                    .then(() => {
-                        callback(null, topic);
+                // #3
+                const authorized = new Authorizer(req.user, topic).update();
+
+                if (authorized) {
+                    // #4
+                    topic.update(updatedTopic, {
+                        fields: Object.keys(updatedTopic)
                     })
-                    .catch((err) => {
-                        callback(err);
-                    });
+                        .then(() => {
+                            callback(null, topic);
+                        })
+                        .catch((err) => {
+                            callback(err);
+                        });
+                } else {
+                    // #5
+                    req.flash("notice", "You are not authorized to do that.");
+                    callback("Forbidden");
+                }
             });
     }
 }
